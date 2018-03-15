2b71b55c3b50cb1ba9b831e25c6cb6c861ab5a6f
Cleiton Pereira <cleiton.workspace@gmail.com>
2018-03-15 17:58:03 -0300

    Opcoes de navegacao da pagina home sao configuradas atraves de uma classe de servico de usuario

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage, NavController, Events } from 'ionic-angular';
+import { AlunoService } from '../../providers/aluno.service';
+import { ProfessorService } from '../../providers/professor.service';
+import { AuthService } from '../../providers/auth.service';
 
 @IonicPage({
   name: 'home',
@@ -10,8 +13,54 @@ import { IonicPage, NavController } from 'ionic-angular';
   templateUrl: 'home.html',
 })
 export class HomePage {
+  public pages: any;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    private events: Events,
+    private auth: AuthService,
+    private alunoService: AlunoService,
+    private professorService: ProfessorService,
+  ) {
+    this.initializePage();
+  }
+
+  private initializePage() {
+    if (this.auth.authenticated()) {
+      this.initializeUserResources();
+    } else {
+      this.getUserResources();
+    }
+  }
+
+  private initializeUserResources() {
+    this.auth.me().subscribe(
+      user => {
+        switch(user.tipo) {
+          case 'aluno': 
+            this.pages = this.alunoService.getNavigationPages();
+            break;
+          case 'professor':
+            this.pages = this.professorService.getNavigationPages();
+            break;
+        }
+      },
+      err => alert('Erro ao obter o usuário logado - status ' + err.status)
+    );
+  }
+
+  private getUserResources() {
+    this.events.subscribe('login', (user, userType) => {
+      switch(userType) {
+        case 'aluno':
+          this.pages = this.alunoService.getNavigationPages();
+          break;
+        case 'professor':
+          this.pages = this.professorService.getNavigationPages();
+          break;
+      }
+    });
+  }
 
   public openPage(pageName) {
     this.navCtrl.push(pageName);
